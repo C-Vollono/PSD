1e822827551ca19f8e04445bdbdc8eb52e76d8d5
AntonioRuotolo <a.ruotolo20@studenti.unisa.it>
2025-05-07 22:46:50 +0200

    Creazione della funzione verificaSconto, correzione aggiornaOrario in veicolo.c

--- Veicolo.h
+++ Veicolo.h
@@ -4,24 +4,24 @@
 
 typedef struct Vettura *veicolo;
 
-void stampaVeicolo (veicolo v, int i);
+void stampaVeicolo (veicolo v, int i); // Check
 
-void modificaDisponibilità (veicolo v, int i, int k); 
+void modificaDisponibilità (veicolo v, int i, int k); // Check
 
-float costoNoleggio (veicolo v, int i, int k);
+float costoNoleggio (veicolo v, int i, int k); // Check
 
-void riempiVeicoli (veicolo v, char* nomefile);
+void riempiVeicoli (veicolo v, char* nomefile); // Check
 
-void riempiOrari (veicolo v, char* nomefile);
+void riempiOrari (veicolo v, char* nomefile); // Check
 
-void stampaOrari (veicolo v, int i);
+void stampaOrari (veicolo v, int i); // Check
 
-void verificaDisponibilità (veicolo v, int i, int k);
+void verificaDisponibilità (veicolo v, int i, int k); // Check
 
-float verificaSconto (veicolo v);
+float verificaSconto (veicolo v, int i, int k); // Check
 
-void aggiornaVeicoli (veicolo v, char* nomefile);
+void aggiornaVeicoli (veicolo v, char* nomefile); // Da disctutere
 
-void aggiornaOrari (veicolo v, char* nomefile, int i, int k);
+void aggiornaOrari (veicolo v, char* nomefile, int i, int k); // Check
 
-void liberaVeicoli (veicolo v);
+void liberaVeicoli (veicolo v); // Check
